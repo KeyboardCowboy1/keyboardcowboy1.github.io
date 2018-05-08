e9954b268976a33b167d136723bda750be76ad63
KeyboardCowboy1 <ecuerden@gmail.com>
2018-05-08 12:12:22 -0400

    swatch generator
    added swatch generator, markup for github projects in future

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,21 @@
- 
+
+document.getElementById("randomColorBtn").addEventListener("click", genRandomColor);
+document.getElementById("randomGradientBtn").addEventListener("click", genRandomGradient);
+
+function genRandomColor() {
+    var rgb = "RGB(" + [Math.floor(256 * Math.random())] + "," + [Math.floor(256 * Math.random())] + "," + [Math.floor(256 * Math.random())] + ")";
+    document.getElementById("generatedSwatch").style.background = rgb;
+    document.getElementById("generatedValues").innerHTML = rgb;
+}
+
+function genRandomGradient() {
+    var angle = Math.floor(181 * Math.random());
+    angle *= 1 == Math.floor(2 * Math.random()) ? 1 : -1;
+    var colOne = "RGB(" + [Math.floor(256 * Math.random())] + "," + [Math.floor(256 * Math.random())] + "," + [Math.floor(256 * Math.random())] + ")"
+      , colTwo = "RGB(" + [Math.floor(256 * Math.random())] + "," + [Math.floor(256 * Math.random())] + "," + [Math.floor(256 * Math.random())] + ")"
+      , gradientString = "linear-gradient(" + angle + "deg," + colOne + "," + colTwo + ")"
+      , gradOutput = "<div id='gradient-info'>" + colOne + "<br>" + colTwo + "<br><span>Angle: " + angle + "&deg;</span></div>";
+      
+    document.getElementById("generatedSwatch").style.background = gradientString,
+    document.getElementById("generatedValues").innerHTML = gradOutput;
+}
